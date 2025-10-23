32c04346c3babff79ff39c7c74e75f23c6cec81f
bhavishy09 <futuresingh111@gmail.com>
2025-10-23 20:36:58 +0530

    minor project

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+  git remote add origin https://github.com/bhavishy09/Hate-HopeSpeech.git
+git branch -M main
+git push -u origin main
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
